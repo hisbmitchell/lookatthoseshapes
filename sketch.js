cd9b15c5cc2cc8281ffd7f9453ef4c509e04f7d1
hisbmitchell <heathermitchell.ottawa@gmail.com>
2017-07-24 14:43:48 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+
+function preload(){
+  bg1 = createVideo (['assets/green1.mp4']);
+  gif = loadGif('assets/shapers3.gif')
+
+ wholething = loadSound('assets/wholething.wav')
+  
+  
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+
+  //shapes.volume(0);
+  bg1.loop();
+  bg1.hide();
+  wholething.play();
+ 
+}
+
+function draw() {
+  
+   //background(0);
+   image(bg1,0,0);
+   //image(shapes,30,30);
+   image(gif, width/4 ,height/4);
+   
+  
+}
